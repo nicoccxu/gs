2d0a6f2f9c52e380738ba2be7e98ef1402190f20
nico <nico.xu@highdatas.com>
2022-03-31 18:00:36 +0800

    接口设置

--- src/main/java/com/ai/gs/config/GsApplication.java
+++ src/main/java/com/ai/gs/config/GsApplication.java
@@ -1,13 +1,15 @@
-package com.ai.gs;
+package com.ai.gs.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.ai")
 @SpringBootApplication
 public class GsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GsApplication.class, args);
+        SpringApplication.run(GsApplication.class,args);
         System.out.println("hello git");
         System.out.println("two git");
         System.out.println("conflict git");
